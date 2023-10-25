2178521cafac67ef4be12ca4d4678d333e780ed9
alexsimenas <109144880+alexsimenas@users.noreply.github.com>
2023-10-25 21:21:42 +0100

    modified files and text

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,12 @@
+
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "My Personal Blog",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Aleksandr Simenas",
+		email: "alexsimenas@gmail.com",
 		url: "https://example.com/about-me/"
 	}
-}
+};
